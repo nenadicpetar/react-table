f9d9e475d731eeaa7d49c971ef06fe345218da61
Petar Nenadić <43269849+nenadicpetar@users.noreply.github.com>
2022-10-28 11:37:25 +0200

    Update index.js
    delete data from personal Firebase Realtime database

--- src/components/firebaseConfig/index.js
+++ src/components/firebaseConfig/index.js
@@ -2,21 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getDatabase } from "firebase/database"
 
 function StartFirebase() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyCvJay1pVgziCYR1MtL2UvmB7RcJ-ctUus",
-        authDomain: "tablica-demo.firebaseapp.com",
-        databaseURL: "https://tablica-demo-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "tablica-demo",
-        storageBucket: "tablica-demo.appspot.com",
-        messagingSenderId: "933549254782",
-        appId: "1:933549254782:web:080677d42f3893e85ed02f",
-        measurementId: "G-CB0T9FMZHR"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    
-    return getDatabase(app);
+    //copy files form Project Overview -> Project settings -> Your apps -> npm
 }
 
 export default StartFirebase;
